d46730a86cae892143438055bb8cc6bcb16a6e72
anwar <anwarioanan@gmail.com>
2024-03-04 01:08:06 -0800

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import Logo from "./images/avatar.jpg";
+import Data from "./data";
+import Header from "./components/header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {Data.map(item => (
+        <Header key={item.id} text={item.text} sub={item.sub} />
+      ))}
     </div>
   );
 }
---
+++ src/components/card.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Cards() {
+    return (
+        <div>
+            <div className="card">
+                <h2>Card 1</h2>
+                <p>This is the content of Card 1.</p>
+            </div>
+            
+        </div>
+    );
+}
+
